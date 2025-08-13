67eae330b410515b6840d5e6c48277c4e824cae0
Eugene Bujak <hmage@hmage.net>
2025-08-13 20:50:57 +0300

    Add simple benchmark.

--- server_test.go
+++ server_test.go
@@ -9,8 +9,10 @@ import (
 	"github.com/miekg/dns"
 )
 
+const testBind = "example.com. 104 A 127.0.0.1\nexample.com. 104 MX 10 mail.example.com."
+
 func ExampleServer() {
-	ts := dnstest.NewServerBind("example.com. 104 A 127.0.0.1\nexample.com. 104 MX 10 mail.example.com.")
+	ts := dnstest.NewServerBind(testBind)
 	defer ts.Close()
 
 	q := dns.Msg{}
@@ -27,7 +29,7 @@ func ExampleServer() {
 }
 
 func TestRoundtrip(t *testing.T) {
-	ts := dnstest.NewServerBind("example.com. 104 A 127.0.0.1\nexample.com. 104 MX 10 mail.example.com.")
+	ts := dnstest.NewServerBind(testBind)
 	defer ts.Close()
 
 	q := dns.Msg{}
@@ -37,8 +39,16 @@ func TestRoundtrip(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	if resp == nil {
+		t.Fatal("Failed to run DNS query: resp == nil")
+	}
+
 	if len(resp.Answer) != 1 {
 		t.Fatalf("Unexpected Answer count: %d", len(resp.Answer))
+	}
+
+	if resp.Rcode != dns.RcodeSuccess {
+		t.Fatalf("DNS error: %s", dns.RcodeToString[resp.Rcode])
 	}
 
 	rr := resp.Answer[0]
@@ -57,5 +67,63 @@ func TestRoundtrip(t *testing.T) {
 
 	if rrA.Hdr.Ttl != 104 {
 		t.Fatalf("Answer is A but TTL isn't 104: %d", rrA.Hdr.Ttl)
+	}
+}
+
+func benchPrep(tb testing.TB, addr string) (*dns.Client, *dns.Conn) {
+	client := &dns.Client{}
+	conn, err := client.Dial(addr)
+	if err != nil {
+		tb.Fatalf("failed to dial DNS server: %s", err)
+	}
+	return client, conn
+}
+
+func benchBody(tb testing.TB, client *dns.Client, conn *dns.Conn) {
+	m := &dns.Msg{}
+	m.SetQuestion("example.com.", dns.TypeA)
+	m.RecursionDesired = true
+
+	_, _, err := client.ExchangeWithConn(m, conn)
+	if err != nil {
+		tb.Fatalf("Failed to exchange with DNS server: %s", err)
+	}
+}
+
+func benchRPS(b *testing.B) {
+	if elapsed := b.Elapsed(); elapsed > 0 {
+		b.ReportMetric(float64(b.N)/elapsed.Seconds(), "RPS")
+	}
+}
+
+func BenchmarkRPS(b *testing.B) {
+	ts := dnstest.NewServerBind(testBind)
+	defer ts.Close()
+	addr := ts.Addr()
+
+	// "single-thread" with single goroutine
+	b.Run("p=0", func(b *testing.B) {
+		client, conn := benchPrep(b, addr)
+		defer conn.Close()
+		for b.Loop() {
+			benchBody(b, client, conn)
+		}
+		benchRPS(b)
+	})
+
+	// "multi-thread" with parallelism*GOMAXPROCS goroutines, this emulates load higher than numCPU
+	var parallelism = []int{1, 2, 4, 8, 16}
+	for _, i := range parallelism {
+		b.Run(fmt.Sprintf("p=%d", i), func(b *testing.B) {
+			b.SetParallelism(i)
+			b.RunParallel(func(pb *testing.PB) {
+				client, conn := benchPrep(b, addr)
+				defer conn.Close()
+				for pb.Next() {
+					benchBody(b, client, conn)
+				}
+			})
+			benchRPS(b)
+		})
 	}
 }
